7dc4e11856aa157a081752ab8848a692bed79ad3
s4006583 <s4006583@student.rmit.edu.au>
2024-05-21 13:54:54 +1000

    set up database

--- DIRT/server/src/database/config.js
+++ DIRT/server/src/database/config.js
@@ -2,6 +2,6 @@ module.exports = {
 	HOST: 'rmit.australiaeast.cloudapp.azure.com',
 	USER: 's4006583_fsd_a2',
 	PASSWORD: 'GreenFrog2005!',
-	DB: 'a2',
+	DB: 's4006583_fsd_a2',
 	DIALECT: 'mysql',
 };
